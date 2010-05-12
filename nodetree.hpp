cff45bcf64d8e02011c16b1b66c752ea07fd30f5
yasir <devnull@localhost>
2010-05-12 13:07:55 -0400

    tree write

--- nodetree.hpp
+++ nodetree.hpp
@@ -2,6 +2,9 @@
 #define NODETREE_HPP
 #include <vector>
 #include <set>
+#include <map>
+#include <stack>
+#include <fstream>
 
 
 class Node{
@@ -10,9 +13,12 @@ class Node{
 		int nid;
 		std::set<int> childSet;
 		std::set<int> vertexSet;
-		Node(int i, int j) {
+		bool isTerm;
+		bool collapsed;
+		Node(int i, int j, bool ist) {
 			nid = i;
 			parent = j;
+			isTerm = ist;
 		}
 };
 		
@@ -21,10 +27,51 @@ class TreeClass{
 		std::map<int, Node*> nodeMap;
 		std::set<int> topLevel;
 		int numNodes;
+		bool writeCompHierEdges(const char* fn);
+		bool writeHRG(const char* fn);
+		bool writeNodeTypes(const char* fn);
 		TreeClass() {
 			numNodes=0;
 		}
 };		
 
+
+bool TreeClass::writeNodeTypes(const char* fn) {
+	std::ofstream file;
+	std::map<int, Node*>::iterator mapit;
+	file.open(fn,std::ios::out);
+	if (not file.is_open()) return 0;
+	for (mapit = nodeMap.begin(); mapit != nodeMap.end() mapit++) {
+		// process the node
+		file << (*mapit).first << "\t";
+		if ((*mapit).second->isTerm) file << "Vertex\n";
+			else file << "Internal\n";
+	}
+};
+
+
+
+bool TreeClass::writeCompHierEdges(const char* fn) {
+	std::stack<int> st;
+	int n;
+	std::ofstream file;
+	std::set<int>::iterator intit,intit2;
+	file.open(fn,std::ios::out);
+	if (not file.is_open()) return 0;
+	for (intit = topLevel.begin(); intit != topLevel.end(); intit++) {
+		st.push(*intit);
+		while ((st.size()>0)) {
+			n = st.top(); st.pop();
+			for (intit2 = nodeMap[n]->childSet.begin(); intit2 != nodeMap[n]->childSet.end(); ++intit2) {
+				st.push(*intit2);
+				// process the element
+				file << n << "\t" << (*intit2) << "\n";
+			}
+		}
+	}
+	file.close();
+	return 1;
+};
+			
 
 #endif
