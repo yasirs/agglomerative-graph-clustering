fad517fa6a9ecd877ef72a0d888c1b1196789423
yasir <devnull@localhost>
2010-05-21 00:25:06 -0400

    correct version

--- residuals.hpp
+++ residuals.hpp
@@ -3,12 +3,12 @@
 
 
 graphData* getResidual(graphData* D, TreeClass* tree, dataMap* w, const int dim) {
-	int d,i,u,v,n1,n2;
+	int d,u,v,n1,n2;
 	std::stack<int> st;
 	graphData *Dnew = new graphData[dim];
 	std::set<int>::iterator intit1, intit2, intit3;
 	std::pair<int,int> ipair;
-	float theta, wthis, wpredicted;
+	float theta, wthis, wpredicted, NE;
 	std::map<int,std::map<int, float*> > topThetas;
 	graphData::destList::iterator eit;
 	for (intit1 = tree->topLevel.begin(); intit1 != tree->topLevel.end(); intit1++) {
@@ -19,13 +19,10 @@ graphData* getResidual(graphData* D, TreeClass* tree, dataMap* w, const int dim)
 			topThetas[n1][n2] = new float[dim];
 			for (d=0;d<dim;d++) {
 				if (D[d].gtype=='w') {
-					theta = (w[d].get_uv(n1,n1) + w[d].get_uv(n2,n2) + w[d].get_uv(n1,n2))/
-						(w[d].selfMissing[n1] + w[d].selfMissing[n2] + (w[d].degrees[n1] * w[d].degrees[n2]) - w[d].get_uv(n1,n2));
-					theta = theta/(theta+1.0f);
+					theta = w[d].get_uv(n1,n2)/(w[d].degrees[n1] * w[d].degrees[n2]);
 					topThetas[n1][n2][d] = theta;
 				} else if (D[d].gtype=='b') {
-					theta = (w[d].get_uv(n1,n1) + w[d].get_uv(n2,n2) + w[d].get_uv(n1,n2))/
-						(w[d].nV[n1] + w[d].nV[n2]);
+					theta = w[d].get_uv(n1,n2)/(w[d].nV[n1] * w[d].nV[n2]);
 					topThetas[n1][n2][d] = theta;
 				} else {
 					std::cout << "graph type "<<D[d].gtype<<" not yet supported for residuals.\n";
@@ -38,8 +35,11 @@ graphData* getResidual(graphData* D, TreeClass* tree, dataMap* w, const int dim)
 		Dnew[d].int2Name = D[d].int2Name;
 		Dnew[d].name2Int = D[d].name2Int;
 		Dnew[d].gtype = 'w';
+		Dnew[d].numV = D[d].numV;
 	}
 	for (d=0;d<dim;d++) {
+		Dnew[d].Etot = 0;
+		NE = 0;
 		for (u=0;u<D[0].numV; u++) {
 			for (eit = D[d].edgeList[u]->begin(); eit != D[d].edgeList[u]->end(); eit++) {
 				v = (*eit).first;
@@ -50,12 +50,22 @@ graphData* getResidual(graphData* D, TreeClass* tree, dataMap* w, const int dim)
 				} else {
 					theta = topThetas[ipair.first][ipair.second][d];
 				}
-				wpredicted = w[d].degrees[u] * w[d].degrees[v] * theta;
+				if (D[d].gtype=='w') {
+					wpredicted = w[d].degrees[u] * w[d].degrees[v] * theta;
+				} else if (D[d].gtype=='b') {
+					wpredicted = w[d].nV[u] * w[d].nV[v] * theta;
+				} else {
+					std::cout << "graph type "<<D[d].gtype<<" not supported yet for computing residuals\n";
+					throw 1;
+				}
 				if ((wthis-wpredicted)>EPS) {
 					Dnew[d].set_uv(u,v,wthis-wpredicted);
+					Dnew[d].Etot += wthis-wpredicted;
+					NE += 1;
 				}
 			}
 		}
+		Dnew[d].aveP = NE/(Dnew[d].numV * Dnew[d].numV);
 	}
 	return Dnew;
 	
